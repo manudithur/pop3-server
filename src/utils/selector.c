b8576e7442d91ed111a3aa115146960250f06a1b
Martin Hexht <mhecht@itba.edu.ar>
2023-06-05 16:26:10 -0300

    state machine and selector bis patch

--- src/utils/selector.c
+++ src/utils/selector.c
@@ -298,10 +298,17 @@ selector_destroy(fd_selector s) {
                 }
             }
             pthread_mutex_destroy(&s->resolution_mutex);
-            for(struct blocking_job *j = s->resolution_jobs; j != NULL;
-                j = j->next) {
-                free(j);
+            // for(struct blocking_job *j = s->resolution_jobs; j != NULL;
+            //     j = j->next) {
+            //     free(j);
+            // }
+            struct blocking_job* j = s->resolution_jobs;
+            while (j != NULL) {
+                struct blocking_job* aux = j;
+                j = j->next;
+                free(aux);
             }
+
             free(s->fds);
             s->fds     = NULL;
             s->fd_size = 0;
@@ -467,18 +474,26 @@ handle_block_notifications(fd_selector s) {
         .s = s,
     };
     pthread_mutex_lock(&s->resolution_mutex);
-    for(struct blocking_job *j = s->resolution_jobs;
-        j != NULL ;
-        j  = j->next) {
+    // for(struct blocking_job *j = s->resolution_jobs;
+    //     j != NULL ;
+    //     j  = j->next) {
+        struct blocking_job* j = s->resolution_jobs;
+        while (j != NULL) {
 
-        struct item *item = s->fds + j->fd;
-        if(ITEM_USED(item)) {
-            key.fd   = item->fd;
+        // struct item *item = s->fds + j->fd;
+        // if(ITEM_USED(item)) {
+        //     key.fd   = item->fd;
+        struct item* item = s->fds + j->fd;
+        if (ITEM_USED(item)) {
+            key.fd = item->fd;
             key.data = item->data;
             item->handler->handle_block(&key);
         }
 
-        free(j);
+        //free(j);
+        struct blocking_job* aux = j;
+        j = j->next;
+        free(aux);
     }
     s->resolution_jobs = 0;
     pthread_mutex_unlock(&s->resolution_mutex);
