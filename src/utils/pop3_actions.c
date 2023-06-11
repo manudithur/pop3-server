06526efc61c4cac9b08d7aca8f67ca94397e5e11
TobiasPerry <tperry@itba.edu.ar>
2023-06-11 20:32:54 -0300

    feat: stat pop3action

--- src/utils/pop3_actions.c
+++ src/utils/pop3_actions.c
@@ -1,5 +1,10 @@
 #include "pop3_actions.h"
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#define PATH_MAX 300
 
 unsigned user_handler(selector_key *key){
 
@@ -10,7 +15,41 @@ unsigned pass_handler(selector_key *key){
 }
 
 unsigned stat_handler(selector_key *key){
+    DIR* directory;
+    client_data * data = ATTACHMENT(key);
+    struct dirent* entry;
+    struct stat fileStat;
+    int count = 0;
+    long long int totalSize = 0;
+    const char *path = "/TODO/enter/mail/dir/path/here";
+    char resultBuffer[256];
 
+    directory = opendir(path);
+    if (directory == NULL) {
+        snprintf(resultBuffer, sizeof(resultBuffer), "ERROR - unable to open mailbox");
+        return ERROR_STATE;
+    }
+    while ((entry = readdir(directory)) != NULL) {
+        if (S_ISREG(fileStat.st_mode)) { 
+            count++;
+        
+            char filePath[PATH_MAX];
+            snprintf(filePath, PATH_MAX, "%s/%s", path, entry->d_name);
+            if (stat(filePath, &fileStat) == 0) {
+                totalSize += fileStat.st_size;
+            }
+        }
+    }
+
+    closedir(directory);
+
+    snprintf(resultBuffer, sizeof(resultBuffer), "+OK %d %lld", count, totalSize);
+    for(int i = 0; i < strlen(resultBuffer); i++){
+        if (buffer_can_write(&data->wbStruct)){
+            buffer_write(&data->wbStruct,resultBuffer[i]);
+        }
+    }
+    return data->stm.current->state;
 }
 
 unsigned list_handler(selector_key *key){
